65aaa4819823e74d2fd3d1a5ce48c8d97ec0a66b
JBrozinski <joseph.brozinski@gmail.com>
2020-02-23 21:17:33 -0700

    html, css, and js code for game

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+var numToGuessEl = $("#number-to-guess");
+var currentScoreEl = $("#current-score");
+var crystalsEl = $("#crystals");
+var messageEl = $("#message");
+var winsEl = $("#wins");
+var lossesEl = $("#losses");
+
+var wins = 0,
+  losses = 0,
+  counter = 0,
+  targetNumber = 0;
+
+startGame();
+
+function getRandomNumber(val) {
+  return Math.floor(Math.random() * val) + 1;
+}
+
+function startGame() {
+  counter = 0;
+  currentScoreEl.text(counter);
+
+  // need to randomize!!!
+  targetNumber = getRandomNumber(50);
+  numToGuessEl.text(targetNumber);
+
+  // create a for loop to create crystals for every numberOption.
+  for (var i = 0; i < crystals.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", crystals[i].img);
+    imageCrystal.attr("data-crystalvalue", getRandomNumber(12));
+    imageCrystal.on("click", crystalClick);
+    crystalsEl.append(imageCrystal);
+  }
+}
+
+function crystalClick() {
+  messageEl.text("Click a crystal to play!");
+  var crystalValue = $(this).attr("data-crystalvalue");
+  crystalValue = parseInt(crystalValue);
+  counter += crystalValue;
+  currentScoreEl.text(counter);
+
+  if (counter === targetNumber) {
+    messageEl.text("You win!");
+    wins++;
+    winsEl.text(wins);
+
+    startGame();
+  } else if (counter >= targetNumber) {
+    messageEl.text("You lose!!");
+    losses++;
+    lossesEl.text(losses);
+
+    startGame();
+  }
+}
