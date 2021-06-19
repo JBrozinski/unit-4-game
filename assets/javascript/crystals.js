2dce669795a322816304b475f75a0ef0d2e7d0ee
JBrozinski <joseph.brozinski@gmail.com>
2021-06-18 18:19:06 -0600

    updated gem image

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -13,6 +13,6 @@ var crystals = [
     },
     {
       img:
-      "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/6/6b/Versus_Crystal.png/revision/latest?cb=20150825214827"
+      "https://am-a.akamaihd.net/image?f=https://news-a.akamaihd.net/public/images/articles/2018/september/ody2018lm/img/loot-gem.png&resize=175:"
     }
   ];
